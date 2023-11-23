334b4f1a93d929bb40e649cf9371ebadd6f0ff3a
Laudial <laudial.dev@gmail.com>
2023-11-23 21:53:43 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
-# Author 1: BEGUE Laurier
-# Author 2: BOULET Kylan
+# Author : BEGUE Laurier
 
 # Importation des bibliothèques
 from src import func as f
 from src import quadraticEquation as qe
 from src import FDEquation as fde
-from src import Interface as IHM
+
+
+def menu():
+    f.clear()
+    print(
+        """
+            Bienvenue dans le programme de résolution d'équation
+[1] Second degré
+[2] Premier degré
+[3] Quitter
+"""
+    )
+
 
 # Programme principal
 if __name__ == "__main__":
-    IHM.runIHM()
+    while True:
+        try:
+            menu()
+            choice = int(input("Votre choix: "))
+            if choice == 1:
+                qe.runQE()
+            elif choice == 2:
+                fde.runEFD()
+            elif choice == 3:
+                f.clear()
+                print("Au revoir")
+                break
+            else:
+                menu()
+        except ValueError:
+            menu()
