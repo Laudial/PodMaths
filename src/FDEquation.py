446a67892ebb216ccb468e8ade34afdfdee3d8a1
Laudial <laudial.dev@gmail.com>
2023-11-24 14:03:15 +0400

    Retrait de la partie tkinter

--- src/FDEquation.py
+++ src/FDEquation.py
@@ -1,67 +1,42 @@
-from tkinter import messagebox
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import tkinter as tk
 from src import func as f
 
 
 def runEFD():
-    root = tk.Tk()
-    root.title("Équation du Premier Degré")
+    f.clear()
 
-    label_a = tk.Label(root, text="Coefficient de x:")
-    label_b = tk.Label(root, text="Terme constant:")
+    a = float(input("Coefficient de x : "))
+    b = float(input("Terme constant : "))
 
-    entry_a = tk.Entry(root)
-    entry_b = tk.Entry(root)
+    f.clear()
 
-    label_a.grid(row=0, column=0, padx=5, pady=5)
-    label_b.grid(row=1, column=0, padx=5, pady=5)
-    entry_a.grid(row=0, column=1, padx=5, pady=5)
-    entry_b.grid(row=1, column=1, padx=5, pady=5)
+    print("\nEquation :", f"{a:.0f}x + {b:.0f} = 0\n")
+    print(equationFD(a, b))
 
-    def solve_first_degree():
-        try:
-            a = float(entry_a.get())
-            b = float(entry_b.get())
-            result = equationFD(a, b)
-            messagebox.showinfo("Résultat", result)
-
-            if messagebox.askyesno("Graphique", "Afficher le graphique ?"):
-                plot_equation(a, b)
-
-        except ValueError:
-            messagebox.showerror(
-                "Erreur", "Veuillez entrer des valeurs numériques valides."
-            )
-
-    solve_button = tk.Button(root, text="Résoudre", command=solve_first_degree)
-    solve_button.grid(row=2, columnspan=2, pady=10)
-
-    root.mainloop()
-
+    graph = input("\nAfficher le graphique ? (O/N) ")
+    if graph.lower() == "o":
+        plot_equation(a, b)
+    else:
+        return None
 
 def plot_equation(a, b):
-    root = tk.Tk()
-    root.title("Graphique de l'Équation du Premier Degré")
+    plt.figure("Equation du Premier Degré")
 
-    fig, ax = plt.subplots()
+    # Le titre
+    plt.title(f"Equation :{a:.0f}x + {b:.0f} = 0")
+    # La courbe
     x = np.linspace(-10, 10, 100)
     y = a * x + b
-    ax.plot(x, y, label=f"{a:.0f}x + {b:.0f}")
-
-    ax.axhline(0, color="black", linewidth=0.5)
-    ax.axvline(0, color="black", linewidth=0.5)
-    ax.set_xlabel("x : abscisse")
-    ax.set_ylabel("y : ordonnée")
-    ax.legend()
-
-    canvas = FigureCanvasTkAgg(fig, master=root)
-    canvas.draw()
-    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-    root.mainloop()
+    plt.plot(x, y, label=f"{a:.0f}x + {b:.0f}")
+    # Les axes
+    plt.axhline(0, color="black", linewidth=0.5)
+    plt.axvline(0, color="black", linewidth=0.5)
+    axes = plt.gca()
+    axes.set_xlabel("x : abscisse")
+    axes.set_ylabel("y : ordonnée")
+    # Afficher
+    plt.show()
 
 
 def equationFD(a, b):
--- src/quadraticEquation.py
+++ src/quadraticEquation.py
@@ -1,80 +1,50 @@
-from tkinter import messagebox
 import math
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import tkinter as tk
 from src import func as f
 
-
 def runQE():
-    root = tk.Tk()
-    root.title("Équation du Second Degré")
+    f.clear()
 
-    label_a = tk.Label(root, text="Valeur de A:")
-    label_b = tk.Label(root, text="Valeur de B:")
-    label_c = tk.Label(root, text="Valeur de C:")
+    a = float(input("Valeur de A: "))
+    b = float(input("Valeur de B: "))
+    c = float(input("Valeur de C: "))
 
-    entry_a = tk.Entry(root)
-    entry_b = tk.Entry(root)
-    entry_c = tk.Entry(root)
+    f.clear()
 
-    label_a.grid(row=0, column=0, padx=5, pady=5)
-    label_b.grid(row=1, column=0, padx=5, pady=5)
-    label_c.grid(row=2, column=0, padx=5, pady=5)
-    entry_a.grid(row=0, column=1, padx=5, pady=5)
-    entry_b.grid(row=1, column=1, padx=5, pady=5)
-    entry_c.grid(row=2, column=1, padx=5, pady=5)
+    print(f"Equation : {a:.0f}x² + {b:.0f}x + {c:.0f}\n")
+    print(deltaCalc(a, b, c))
 
-    def solve_second_degree():
-        try:
-            a = float(entry_a.get())
-            b = float(entry_b.get())
-            c = float(entry_c.get())
-            result = deltaCalc(a, b, c)
-            messagebox.showinfo("Résultat", result)
-
-            if messagebox.askyesno("Graphique", "Afficher le graphique ?"):
-                plot_trinome(a, b, c)
-
-        except ValueError:
-            messagebox.showerror(
-                "Erreur", "Veuillez entrer des valeurs numériques valides."
-            )
-
-    solve_button = tk.Button(root, text="Résoudre", command=solve_second_degree)
-    solve_button.grid(row=3, columnspan=2, pady=10)
-
-    root.mainloop()
-
-def plot_trinome(a, b, c):
-    root = tk.Tk()
-    root.title(f"Trinôme {a:.0f}x² + {b:.0f}x + {c:.0f}")
-
-    fig, ax = plt.subplots()
-    x = np.linspace(-10, 10, 100)
-    y = trinome(a, b, c, x)
-    ax.plot(x, y, label=f"{a:.0f}x² + {b:.0f}x + {c:.0f}")
-
-    ax.axhline(0, color="r")
-    ax.axvline(0, color="r")
-    ax.set_xlabel("x : abscisse")
-    ax.set_ylabel("f(x) : ordonnée")
-    ax.legend()
-
-    canvas = FigureCanvasTkAgg(fig, master=root)
-    canvas.draw()
-    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-    root.mainloop()
+    graph = input("\nAfficher le graphique ? (O/N)")
+    if graph.lower() == "o":
+        plot_trinome(a, b, c)
+    else:
+        return None
 
 def trinome(a, b, c, x):
     return a * x**2 + b * x + c
 
+def plot_trinome(a, b, c):
+    plt.figure(f"Trinôme {a:.0f}x² + {b:.0f}x + {c:.0f}")
+
+    # Le titre
+    plt.title(f"f(x) = {a:.0f}x² + {b:.0f}x + {c:.0f}")
+
+    # L'intervalle de trace
+    x = np.linspace(-10, 10, 100)
+    # La courbe
+    plt.plot(x, trinome(a, b, c, x))
+    # Les axes
+    plt.axvline(x=0, color="r")
+    plt.axhline(y=0, color="r")
+    axes = plt.gca()
+    axes.set_xlabel("x : abscisse")
+    axes.set_ylabel("f(x) : ordonnée")
+    # Afficher
+    plt.show()
 
 def discriminant(a, b, c):
     return b**2 - 4 * a * c
-
 
 def deltaCalc(a, b, c):
     delta = discriminant(a, b, c)
@@ -86,4 +56,7 @@ def deltaCalc(a, b, c):
         s0 = -b / (2 * a)
         return f"Solution : {s0:.2f}"
     else:
-        return
+        return "Aucune solution"
+
+if __name__ == "__main__":
+    runQE()
